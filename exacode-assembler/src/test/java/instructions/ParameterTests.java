4f9cb1ba76bdfdb608e8ca7fae350d4a3c17ab8f
PennyJim <40438818+PennyJim@users.noreply.github.com>
2023-09-08 13:07:20 -0600

    Split Value Test

--- exacode-assembler/src/test/java/instructions/ParameterTests.java
+++ exacode-assembler/src/test/java/instructions/ParameterTests.java
@@ -1,10 +1,10 @@
 package instructions;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,11 +14,12 @@ import com.pennyjim.exacode.Parameter.Types;
 
 public class ParameterTests {
 	private static Class<InvalidParameterException> exception = InvalidParameterException.class;
+	private static int randomCount = 100;
 	
+	@Disabled
 	@Test
-	@DisplayName("Value test")
-	void ValueTest() {
-		final int randomCount = 100;
+	@DisplayName("Old Random Value test")
+	void RandValueTest() {
 		for (int i = 0; i < randomCount; i++) {
 			int randomValue = (int)(Math.random() * 20000);
 			int randomReg = randomValue % 20;
@@ -56,6 +57,88 @@ public class ParameterTests {
 				assertEquals(possiblyNegative, testValue.getValue(), "Value does not match expected");
 			}
 		}
+	}
+
+	@Test
+	@DisplayName("Random Register Test")
+	void RandomRegTest() {
+		for (int i = 0; i < randomCount; i++) {
+			int randomValue = (int)(Math.random() * 20);
+
+			if (randomValue > 9) {
+				assertThrows(exception, () -> {new Parameter("#"+randomValue);});
+			} else {
+				Parameter testRegister = new Parameter("#"+randomValue);
+
+				assertEquals(Types.REGISTER, testRegister.getType(), "Is not a REGISTER");
+				assertEquals(randomValue, testRegister.getValue(), "Value does not match expected");
+			}
+		}
+	}
+
+	@Test
+	@DisplayName("Random Memory Test")
+	void RandomMemTest() {
+		for (int i = 0; i < randomCount; i++) {
+			int randomValue = (int)(Math.random() * 20000);
+
+			if (randomValue > 9999) {
+				assertThrows(exception, () -> {new Parameter("&"+randomValue);});
+			} else {
+				Parameter testMemory = new Parameter("&"+randomValue);
+
+				assertEquals(Types.MEMORY, testMemory.getType(), "Is not a MEMORY");
+				assertEquals(randomValue, testMemory.getValue(), "Value does not match expected");
+			}
+		}
+	}
+
+	@Test
+	@DisplayName("Random Line Test")
+	void RandomLineTest() {
+		for (int i = 0; i < randomCount; i++) {
+			int randomValue = (int)(Math.random() * 10000);
+
+			if (randomValue > 3333) {
+				assertThrows(exception, () -> {new Parameter("!"+randomValue);});
+			} else {
+				Parameter testLine = new Parameter("!"+randomValue);
+
+				assertEquals(Types.LINE, testLine.getType(), "Is not a LINE");
+				assertEquals(randomValue, testLine.getValue(), "Value does not match expected");
+			}
+		}
+	}
+
+	@Test
+	@DisplayName("Random Value Test")
+	void RandomValTest() {
+		for (int i = 0; i < randomCount; i++) {
+			int randomValue = (int)(Math.random() * 40000) - 20000;
+
+			if (randomValue < -9999 || randomValue > 9999) {
+				assertThrows(exception, () -> {new Parameter(Integer.toString(randomValue));});
+			} else {
+				Parameter testValue = new Parameter(Integer.toString(randomValue));
+
+				assertEquals(Types.VALUE, testValue.getType(), "Is not a VALUE");
+				assertEquals(randomValue, testValue.getValue(), "Value does not match expected");
+			}
+		}
+	}
+
+	@Test
+	@DisplayName("Static Value Test")
+	void StaticValueTest() {
+		Parameter result = new Parameter("#RES");
+		assertEquals(Types.RESULT, result.getType(), "Is not a RESULT");
+
+		assertThrows(exception, () -> {new Parameter("#RESULT");}, "Shouldn't have worked");
+		assertThrows(exception, () -> {new Parameter("VARIABLE");}, "Shouldn't have worked");
+		assertThrows(exception, () -> {new Parameter("#O");}, "Shouldn't have worked");
+		assertThrows(exception, () -> {new Parameter("what");}, "Shouldn't have worked");
+		assertThrows(exception, () -> {new Parameter("%TEST");}, "Shouldn't have worked");
+		assertThrows(exception, () -> {new Parameter("ILLOGIC");}, "Shouldn't have worked");
 	}
 
 	@Test
