862f951bbe96bcdd1e6598e0de8119deda623a42
PennyJim <40438818+PennyJim@users.noreply.github.com>
2023-09-08 14:50:39 -0600

    Use new method

--- exacode-assembler/src/main/java/com/pennyjim/exacode/instructions/MathInstruction.java
+++ exacode-assembler/src/main/java/com/pennyjim/exacode/instructions/MathInstruction.java
@@ -39,92 +39,42 @@ public abstract class MathInstruction extends Instruction {
 		int RegRes,
 		int ResReg
 	) {
-		this(instName,param1,param2,lineNum,verb,
-			ValVal, ResRes, ResVal, ValRes, RegReg, RegVal, ValReg, RegRes, ResReg, false);
-	}
-
-	public MathInstruction(
-		String instName,
-		Parameter param1, 
-		Parameter param2, 
-		int lineNum,
-		String verb,
-		
-		int ValVal,
-		int ResRes,
-		int ResVal,
-		int ValRes,
-		int RegReg,
-		int RegVal,
-		int ValReg,
-		int RegRes,
-		int ResReg,
-		boolean isOld
-	) {
 		super(instName, param1, param2, lineNum, 2, verb);
 		if (this.errMsg != "") return; //Already errored;
-		if (isOld) {
-			//Old if else if implementation
-			if (param1.getType() == Types.VALUE && param2.getType() == Types.VALUE) {
+		//New 2D switch case implementation
+		int selector = param1.getType().ordinal() * Types.values().length + param2.getType().ordinal();
+		switch (selector) {
+			case 0 :	//	Value				Value
 				this.instNum = ValVal;
-			} else if (param1.getType() == Types.RESULT && param2.getType() == Types.RESULT) {
+				break;
+			case 14:	//	Result			Result
 				this.instNum = ResRes;
-			} else if (param1.getType() == Types.RESULT && param2.getType() == Types.VALUE) {
+				break;
+			case 12:	//	Result			Value
 				this.instNum = ResVal;
-			} else if (param1.getType() == Types.VALUE && param2.getType() == Types.RESULT) {
+				break;
+			case 2 :	//	Value				Result
 				this.instNum = ValRes;
-			} else if (param1.getType() == Types.REGISTER && param2.getType() == Types.REGISTER) {
+				break;
+			case 7 :	//	Register		Register
 				this.instNum = RegReg;
-			} else if (param1.getType() == Types.REGISTER && param2.getType() == Types.VALUE) {
+				break;
+			case 6 :	//	Register		Value
 				this.instNum = RegVal;
-			} else if (param1.getType() == Types.VALUE && param2.getType() == Types.REGISTER) {
+				break;
+			case 1 :	//	Value				Register
 				this.instNum = ValReg;
-			} else if (param1.getType() == Types.REGISTER && param2.getType() == Types.RESULT) {
+				break;
+			case 8 :	//	Register		Result
 				this.instNum = RegRes;
-			} else if (param1.getType() == Types.RESULT && param2.getType() == Types.REGISTER) {
-				this.instNum = ResReg;
-			}
+				break;
+			case 13:	//	Result			Register
+				this.instNum = ResReg;				
+				break;
 
-			if (this.instNum == -100) {
+			default: //		Other
 				this.errMsg = param1.getType()+" and "+param2.getType()+" are not currently supported parameter types";
-			}
-
-		} else {
-			//New 2D switch case implementation
-			int selector = param1.getType().ordinal() * Types.values().length + param2.getType().ordinal();
-			switch (selector) {
-				case 0 :	//	Value				Value
-					this.instNum = ValVal;
-					break;
-				case 14:	//	Result			Result
-					this.instNum = ResRes;
-					break;
-				case 12:	//	Result			Value
-					this.instNum = ResVal;
-					break;
-				case 2 :	//	Value				Result
-					this.instNum = ValRes;
-					break;
-				case 7 :	//	Register		Register
-					this.instNum = RegReg;
-					break;
-				case 6 :	//	Register		Value
-					this.instNum = RegVal;
-					break;
-				case 1 :	//	Value				Register
-					this.instNum = ValReg;
-					break;
-				case 8 :	//	Register		Result
-					this.instNum = RegRes;
-					break;
-				case 13:	//	Result			Register
-					this.instNum = ResReg;				
-					break;
-
-				default: //		Other
-					this.errMsg = param1.getType()+" and "+param2.getType()+" are not currently supported parameter types";
-					break;
-			}
+				break;
 		}
 	}
 }
