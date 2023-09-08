3493a27a27d14e7aae951fde3339a9253477acba
PennyJim <40438818+PennyJim@users.noreply.github.com>
2023-09-08 15:10:13 -0600

    Remove old and broken test

--- exacode-assembler/src/test/java/instructions/ParameterTests.java
+++ exacode-assembler/src/test/java/instructions/ParameterTests.java
@@ -16,49 +16,6 @@ import com.pennyjim.exacode.Parameter.Types;
 public class ParameterTests {
 	private static Class<InvalidParameterException> exception = InvalidParameterException.class;
 	private static int randomCount = 100;
-	
-	@Disabled
-	@Test
-	@DisplayName("Old Random Value test")
-	void RandValueTest() {
-		for (int i = 0; i < randomCount; i++) {
-			int randomValue = (int)(Math.random() * 20000);
-			int randomReg = randomValue % 20;
-
-			if (randomReg > 9) {
-				assertThrows(exception, () -> {new Parameter("#"+randomReg);});
-			} else {
-				Parameter testRegister = new Parameter("#"+randomReg);
-
-				assertEquals(Types.REGISTER, testRegister.getType(), "Is not a REGISTER");
-				assertEquals(randomReg, testRegister.getValue(), "Value does not match expected");
-			}
-
-			if (randomValue > 9999) {
-				assertThrows(exception, () -> {new Parameter("&"+randomValue);});
-				assertThrows(exception, () -> {new Parameter("!"+randomValue);});
-			} else {
-				Parameter testMemory = new Parameter("&"+randomValue);
-				Parameter testLine = new Parameter("!"+randomValue);
-
-				assertEquals(Types.MEMORY, testMemory.getType(), "Is not a MEMORY");
-				assertEquals(randomValue, testMemory.getValue(), "Value does not match expected");
-
-				assertEquals(Types.LINE, testLine.getType(), "Is not a LINE");
-				assertEquals(randomValue, testLine.getValue(), "Value does not match expected");
-			}
-
-			int possiblyNegative = (int)(Math.random() * 30000) - 10000;
-			if (possiblyNegative < -9999 || possiblyNegative > 9999) {
-				assertThrows(exception, () -> {new Parameter(Integer.toString(possiblyNegative));});
-			} else {
-				Parameter testValue = new Parameter(Integer.toString(possiblyNegative));
-
-				assertEquals(Types.VALUE, testValue.getType(), "Is not a VALUE");
-				assertEquals(possiblyNegative, testValue.getValue(), "Value does not match expected");
-			}
-		}
-	}
 
 	@Test
 	@DisplayName("Random Register Test")
